10abc27bb1df233274d62a704dbcced4fd8c4480
seb26 <unleashed26@gmail.com>
2011-08-13 22:52:15 +1000

    Changes on Python end. hllib.py uncommented parts work perfectly :D finally.

--- hllib.py
+++ hllib.py
@@ -6,4 +6,38 @@ import ctypes
 import chllib
 
 class HLLib:
-    pass
+
+
+    def __init__(self, package, volatile=False):
+        self.package = package
+        self.volatile = volatile
+
+
+    def __open__(self):
+        self.pkg = chllib.Package(self.package, volatileaccess=self.volatile)
+        return True
+
+
+    def __close__(self):
+        if self.pkg:
+            self.pkg.close()
+        else:
+            return None
+
+
+    def extract(self, extr, outdir):
+        """
+        self.__open__()
+        for z in extr:
+            self.pkg.extract(z, outdir)
+        self.__close__()
+        """
+        return outdir
+
+
+# HL = HLLib(r'C:\Program Files\Steam\steamapps\team fortress 2 content.gcf')
+
+print chllib.Package(r'C:\Program Files\Steam\steamapps\team fortress 2 content.gcf', volatileaccess=True).extract(r'root\tf\steam.inf', r'X:\test01')
+
+# print HL.extract([ r'root\tf\steam.inf', r'root\tf\scripts\items\items_game.txt' ], r'X:\test01')
+
