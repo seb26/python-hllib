0be264d1d841ca64dfb6eb7ecb3f798672356cf2
seb26 <unleashed26@gmail.com>
2011-08-13 20:22:35 +1000

    Initial commit.

---
+++ hllib.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2011 seb26. All rights reserved.
+# Source code is licensed under the terms of the Modified BSD License.
+
+import os
+import ctypes
+import chllib
+
+class HLLib:
+    pass
