4c5579b48b3e5edbcd8b8c7a85b816b4f9337c8b
seb26 <unleashed26@gmail.com>
2011-08-14 13:18:50 +1000

    Added extract() function.

--- hllib.py
+++ hllib.py
@@ -4,13 +4,18 @@
 import os
 import ctypes
 import chllib
+from collections import defaultdict
 
 class HLLib:
 
 
     def __init__(self, package, volatile=False):
-        self.package = package
         self.volatile = volatile
+        self.package = package
+        if os.path.splitext(package)[1] == '.gcf':
+            self.packagestr = package.split('\\')[-1] # the filename of the gcf
+        else:
+            self.packagestr = package.split('\\')[-2] # the parent directory of the package
 
 
     def __open__(self):
@@ -21,23 +26,28 @@ class HLLib:
     def __close__(self):
         if self.pkg:
             self.pkg.close()
+            return True
         else:
             return None
 
 
-    def extract(self, extr, outdir):
-        """
-        self.__open__()
-        for z in extr:
-            self.pkg.extract(z, outdir)
-        self.__close__()
-        """
-        return outdir
-
-
-# HL = HLLib(r'C:\Program Files\Steam\steamapps\team fortress 2 content.gcf')
-
-print chllib.Package(r'C:\Program Files\Steam\steamapps\team fortress 2 content.gcf', volatileaccess=True).extract(r'root\tf\steam.inf', r'X:\test01')
-
-# print HL.extract([ r'root\tf\steam.inf', r'root\tf\scripts\items\items_game.txt' ], r'X:\test01')
-
+    def extract(self, extr, outdir, **kwargs):
+        if type(extr) is list:
+            if 'multidir' in kwargs and kwargs['multidir'] is True:
+                edir = {}
+                temp = defaultdict(list)
+                for z in extr:
+                    psplit = z.split('\\')
+                    wdir = os.path.split(z)[0]
+                    temp[wdir].append(z) # Add each fn to the appropriate working dir list.
+                self.__open__()
+                for t in temp.items():
+                    extr_dir = os.path.join(outdir, self.packagestr, t[0])
+                    if not os.path.exists(extr_dir):
+                        os.makedirs(extr_dir)
+                    if len(t[1]) > 1:
+                        for f in t[1]:
+                            self.pkg.extract(f, extr_dir)
+                    else:
+                        self.pkg.extract(t[1], extr_dir)
+                self.__close__()
